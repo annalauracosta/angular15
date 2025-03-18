d3b9469658be6835d7729cc9f033bbf54468021b
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-18 17:49:43 -0300

    app.module.ts

--- cadastro-cliente/src/app/app.module.ts
+++ cadastro-cliente/src/app/app.module.ts
@@ -1,24 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import { ClientesModule } from './clientes/clientes.module';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { ClientesListarComponent } from './clientes-listar/clientes-listar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroClienteComponent,
+    ClientesListarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
     MatButtonModule,
-    ClientesModule
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    FormsModule
   ],
   providers: [
     provideAnimationsAsync()
@@ -26,3 +40,4 @@ import { ClientesModule } from './clientes/clientes.module';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
