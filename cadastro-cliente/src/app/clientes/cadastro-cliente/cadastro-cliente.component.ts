508b2a9adb7be9a692090c700a5891a02f25eec9
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-18 19:37:54 -0300

    cadastro-cliente.component.ts

--- cadastro-cliente/src/app/clientes/cadastro-cliente/cadastro-cliente.component.ts
+++ cadastro-cliente/src/app/clientes/cadastro-cliente/cadastro-cliente.component.ts
@@ -13,8 +13,8 @@ export class CadastroClienteComponent {
   constructor(private router: Router) {}
 
   register() {
-    console.log('Nome:', this.name);
-    console.log('Idade:', this.age);
+    console.log('Nome:', this.name); // <--- AQUI: Exibe o nome no console
+    console.log('Idade:', this.age); // <--- AQUI: Exibe a idade no console
     alert(`Registro realizado com sucesso!\nNome: ${this.name}\nIdade: ${this.age}`);
   }
 
