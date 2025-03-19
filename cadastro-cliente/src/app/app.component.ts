249d463fdab8ca2b9f0c2d44beb8daa81a6a7eed
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-19 12:11:07 -0300

    app.component.ts

--- cadastro-cliente/src/app/app.component.ts
+++ cadastro-cliente/src/app/app.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
- import { Router } from '@angular/router';
- 
- @Component({
-   selector: 'app-root',
-   templateUrl: './app.component.html',
-   styleUrl: './app.component.css'
- })
- export class AppComponent {
-   title = 'cadastro-cliente';
- 
-   constructor(private router: Router) {}
-     
-       goToRegister() {
-         this.router.navigate(['/registrar']);
-       }
-     
- }
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'] 
+})
+export class AppComponent {
+  title = 'cadastro-cliente';
+
+  constructor(private router: Router) {}
+
+  goToRegister() {
+    this.router.navigate(['/registrar']);
+  }
+}
