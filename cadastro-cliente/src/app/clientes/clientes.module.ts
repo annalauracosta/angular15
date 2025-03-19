fb4ec47deff3af200d8292ad5d36c1134bddde35
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-18 22:11:06 -0300

    clientes.module.ts

--- cadastro-cliente/src/app/clientes/clientes.module.ts
+++ cadastro-cliente/src/app/clientes/clientes.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientesListarComponent } from './clientes-listar/clientes-listar.component';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import { ClientesListarComponent } from './clientes-listar/clientes-listar.component';
+import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-
-
-
-
 
 @NgModule({
   declarations: [
@@ -22,7 +16,14 @@ import { MatListModule } from '@angular/material/list';
     ClientesListarComponent
   ],
   imports: [
-    CommonModule,FormsModule,MatFormFieldModule,MatButtonModule,MatCardModule,MatInputModule,MatButtonModule,MatIconModule,MatListModule
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatListModule
   ]
 })
 export class ClientesModule { }
