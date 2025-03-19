30ff2701767b5058eb0850008b253709ccdb9647
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-19 00:07:37 -0300

    app-routing.module.ts

--- cadastro-cliente/src/app/app-routing.module.ts
+++ cadastro-cliente/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
-import { ClientesListarComponent } from './clientes-listar/clientes-listar.component';
+import { CadastroClienteComponent } from './clientes/cadastro-cliente/cadastro-cliente.component';
+import { ClientesListarComponent } from './clientes/clientes-listar/clientes-listar.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/clientes', pathMatch: 'full' },
-  { path: 'registrar', component: CadastroClienteComponent },
-  { path: 'clientes', component: ClientesListarComponent }
+  {path:'',redirectTo:'/clientes',pathMatch:'full'},
+  {
+    path:'registrar',component: CadastroClienteComponent
+  },
+  {
+    path:'clientes',component:ClientesListarComponent
+  }
 ];
 
 @NgModule({
