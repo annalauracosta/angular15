ed6d614a17014704806d6de5141b69f30c948c7c
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-21 10:49:26 -0300

    clientes-listar.component.ts

--- cadastro-cliente/src/app/clientes/clientes-listar/clientes-listar.component.ts
+++ cadastro-cliente/src/app/clientes/clientes-listar/clientes-listar.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+
  import { Router } from '@angular/router';
  @Component({
    selector: 'app-clientes-listar',
    templateUrl: './clientes-listar.component.html',
-    styleUrls: ['./clientes-listar.component.css']
-
+   styleUrl: './clientes-listar.component.css'
  })
  export class ClientesListarComponent {
    customers = [
