84ad4f88f74faa9be4c7b2197e9cdf9fd866864d
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-28 11:03:50 -0300

    cadastro-cliente.component.ts

--- cadastro-cliente/src/app/clientes/cadastro-cliente/cadastro-cliente.component.ts
+++ cadastro-cliente/src/app/clientes/cadastro-cliente/cadastro-cliente.component.ts
@@ -4,39 +4,20 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-cadastro-cliente',
   templateUrl: './cadastro-cliente.component.html',
-  styleUrls: ['./cadastro-cliente.component.css'] // Correção aqui
+  styleUrls: ['./cadastro-cliente.component.css']
 })
 export class CadastroClienteComponent {
   name: string = '';
   age: number | null = null;
 
-
+  
   register() {
     console.log('Nome:', this.name);
     console.log('Idade:', this.age);
     alert(`Registro realizado com sucesso!\nNome: ${this.name}\nIdade: ${this.age}`);
   }
 
-  import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-
-@Component({
-  selector: 'app-cadastro-cliente',
-  templateUrl: './cadastro-cliente.component.html',
-  styleUrls: ['./cadastro-cliente.component.css'] // Correção aqui
-})
-export class CadastroClienteComponent {
-  name: string = '';
-  age: number | null = null;
-
-
-  register() {
-    console.log('Nome:', this.name);
-    console.log('Idade:', this.age);
-    alert(`Registro realizado com sucesso!\nNome: ${this.name}\nIdade: ${this.age}`);
-  }
-
-   constructor(private router: Router) {}
+  constructor(private router: Router) {}
 
   goToList() {
     this.router.navigate(['/clientes']);
