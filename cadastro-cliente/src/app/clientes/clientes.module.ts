85b177e740888d0d6521551849b3c682cfe54b33
annalauracosta <146785688+annalauracosta@users.noreply.github.com>
2025-03-28 10:54:06 -0300

    clientes.module.ts

--- cadastro-cliente/src/app/clientes/clientes.module.ts
+++ cadastro-cliente/src/app/clientes/clientes.module.ts
@@ -1,28 +1,32 @@
 import { NgModule } from '@angular/core';
- import { CommonModule } from '@angular/common';
- import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
- 
- import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
- import { MatCardModule } from '@angular/material/card';
- import { MatInputModule } from '@angular/material/input';
- import { MatButtonModule } from '@angular/material/button';
- import { MatFormFieldModule } from '@angular/material/form-field';
- import { FormsModule } from '@angular/forms';
- import {MatIconModule} from '@angular/material/icon';
- import { ClientesListarComponent } from './clientes-listar/clientes-listar.component';
- import { MatListModule } from '@angular/material/list';
- 
- 
- 
- 
- 
- @NgModule({
-   declarations: [
-     CadastroClienteComponent,
-     ClientesListarComponent
-   ],
-   imports: [
-     CommonModule,FormsModule,MatFormFieldModule,MatButtonModule,MatCardModule,MatInputModule,MatButtonModule,MatIconModule,MatListModule
-   ]
- })
- export class ClientesModule { }
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CadastroClienteComponent } from './cadastro-cliente/cadastro-cliente.component';
+import { ClientesListarComponent } from './clientes-listar/clientes-listar.component';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  declarations: [
+    CadastroClienteComponent,
+    ClientesListarComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatListModule
+  ]
+})
+export class ClientesModule { }
+
